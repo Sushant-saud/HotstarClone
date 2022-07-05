6a24616d070b8d1a427163db91950ef36022bd31
Sushant-saud <98227002+Sushant-saud@users.noreply.github.com>
2022-07-05 22:46:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@ let movies =[
     {
         name: "falcon and winter soldier",
         des: "ectetur adipisicirepellendus quo est maxime aspeure",
-        image:"/images/Img/slider 2.png"
+        image:"images/Img/slider 2.png"
     },
     {
         name: "Loki",
         des:"m ipsum dolor sit amet consectetur adipisicin",
-        image: "/images/Img/slider 1.png"
+        image: "images/Img/slider 1.png"
     },
     {
         name: "Wanda Vision",
         des: "Lorem ipsum dolor sit ame. Ullam s aspernatur qui iure",
-        image: "/images/Img/slider 3.png"
+        image: "images/Img/slider 3.png"
     },
     {
         name: "Raya and the last dragon",
         des: "Lorem ipsum dolor sit ame. Ullam s quo est maaturre",
-        image: "/images/Img/slider 4.png"
+        image: "images/Img/slider 4.png"
     },
     {
         name: "Luca",
