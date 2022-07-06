bea88356c99287ea5070c70d778e88bd5c8e8ea2
Sushant-saud <98227002+Sushant-saud@users.noreply.github.com>
2022-07-06 12:24:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,27 +2,27 @@ let movies =[
     {
         name: "falcon and winter soldier",
         des: "ectetur adipisicirepellendus quo est maxime aspeure",
-        image:"images/Img/poster 1.png"
+        image:"images/Img/slider 1.png"
     },
     {
         name: "Loki",
         des:"m ipsum dolor sit amet consectetur adipisicin",
-        "images/Img/poster 2.png"
+        "images/Img/slider 2.png"
     },
     {
         name: "Wanda Vision",
         des: "Lorem ipsum dolor sit ame. Ullam s aspernatur qui iure",
-      image:"images/Img/poster 3.png"
+      image:"images/Img/slider 3.png"
     },
     {
         name: "Raya and the last dragon",
         des: "Lorem ipsum dolor sit ame. Ullam s quo est maaturre",
-        image:"images/Img/poster 4.png"
+        image:"images/Img/slider 4.png"
     },
     {
         name: "Luca",
         des: "Lorem ipsum dolor sit amet consectetu repellma qui iure",
-        image:"images/Img/poster 5.png"
+        image:"images/Img/slider 5.png"
 
 ];
 const carousel = document.querySelector(".caraousel");
